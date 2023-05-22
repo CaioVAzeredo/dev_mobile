8be2c8151da5c6297bc60fc6ef0791df5f112930
Caio Viana de Azeredo <122998245+CaioVAzeredo@users.noreply.github.com>
2023-05-22 20:09:37 -0300

    pausa sobre dev mobile

--- curso_flutter/projeto_perguntas/lib/main.dart
+++ curso_flutter/projeto_perguntas/lib/main.dart
@@ -1,3 +1,67 @@
-main() {
-  print('primeiro programa');
+// ignore_for_file: avoid_print
+
+import 'package:flutter/material.dart';
+
+main() => runApp(PerguntaApp());
+
+class PerguntaAppState extends State<PerguntaApp> {
+  var perguntaSelecionada = 0;
+
+  void responder() {
+    setState(() {
+      perguntaSelecionada++;
+    });
+    print(perguntaSelecionada);
+  }
+
+  final List<String> perguntas = [
+    'Qual sua cor favorita',
+    'Qual é seu animal favorito',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    void responder() {
+      print('Pergunta respondida!!');
+    }
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Perguntas',
+          ),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text(perguntas[0]),
+            ElevatedButton(
+              onPressed: responder,
+              child: Text(
+                  'Resposta 1'), //poderia ser tbm "onPressed: () => {responder()}"
+            ),
+            ElevatedButton(
+              onPressed: responder,
+              child: Text('Resposta 2'), //Chamando direto
+            ),
+            ElevatedButton(
+              child: Text('Resposta 3'),
+              onPressed: () => responder, //utilizando arrowFunction
+            ),
+            ElevatedButton(
+              child: Text('Resposta 4'),
+              onPressed: () {
+                print('Resposta 2 foi selecionada!!');
+              }, //Função ja dentro do onPressed
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PerguntaApp extends StatefulWidget {
+  @override
+  State<PerguntaApp> createState() => PerguntaAppState();
 }
